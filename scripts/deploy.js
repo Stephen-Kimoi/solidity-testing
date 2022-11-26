5fc54660eb77fbe7b0a3a28057abb828623bc6bd
Steve Kimoi <moenisteve@gmail.com>
2022-11-26 14:06:51 +0300

    checking whether the contract can be deployed

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat");
+
+async function main () {
+   const [ deployer ]  = await ethers.getSigners(); 
+   console.log("Deploying contract with the account: ", deployer.address);
+
+   const accountBalance = await (deployer.getBalance()).toString(); 
+   console.log("Account balance: ", accountBalance); 
+
+
+   const tokenContract = await ethers.getContractFactory("Token"); 
+   const deployedTokenContract = await tokenContract.deploy(); 
+   https://www.alchemyapi.io
+   await deployedTokenContract.deployed(); 
+
+   console.log("Contract address: ", deployedTokenContract.address); 
+} 
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
